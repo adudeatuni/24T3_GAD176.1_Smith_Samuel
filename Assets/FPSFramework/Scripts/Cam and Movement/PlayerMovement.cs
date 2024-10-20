aca674391ebc5e1c73d28256e41806137154b26f
adudeatuni <159965809+adudeatuni@users.noreply.github.com>
2024-10-20 20:34:47 +1100

    Tidying and neatly packaging assets

--- Assets/FPSFramework/Scripts/Cam and Movement/PlayerMovement.cs
+++ Assets/FPSFramework/Scripts/Cam and Movement/PlayerMovement.cs
@@ -20,11 +20,11 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 moveDirection;
 
-    Rigidbody rb;
+    Rigidbody rigidbod;
     private void Start()
     {   //Gets reference to the player rigidbody, then freezes the player's rotation (prevents them falling over)
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        rigidbod = GetComponent<Rigidbody>();
+        rigidbod.freezeRotation = true;
     }
 
     // Update is called once per frame
@@ -37,11 +37,11 @@ public class PlayerMovement : MonoBehaviour
         //sets the drag if player is grounded
         if (grounded)
         {
-            rb.drag =groundDrag;
+            rigidbod.drag =groundDrag;
         }
         else
         {
-            rb.drag = 0;
+            rigidbod.drag = 0;
         }
 
         SpeedControl();
@@ -63,18 +63,18 @@ public class PlayerMovement : MonoBehaviour
         //calculate movement direction, will always walk direction player is looking
         moveDirection = orentation.forward * verticalInput + orentation.right * horizontalInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 15f, ForceMode.Force);
+        rigidbod.AddForce(moveDirection.normalized * moveSpeed * 15f, ForceMode.Force);
     }
 
     private void SpeedControl()
     {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        Vector3 flatVel = new Vector3(rigidbod.velocity.x, 0f, rigidbod.velocity.z);
 
         //if needed, limit player's velocity
         if(flatVel.magnitude > moveSpeed)
         {
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            rigidbod.velocity = new Vector3(limitedVel.x, rigidbod.velocity.y, limitedVel.z);
         }
     }
 }
