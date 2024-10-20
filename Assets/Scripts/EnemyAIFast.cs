5cf51be804738d1adeb8f7510d292789ce0b2de2
adudeatuni <159965809+adudeatuni@users.noreply.github.com>
2024-10-20 20:17:16 +1100

    FastAI , uses OnTriggerEnter function inherited from Enemy AI

--- Assets/Scripts/EnemyAIFast.cs
+++ Assets/Scripts/EnemyAIFast.cs
@@ -2,25 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAIRandom : EnemyAI
 {
-    protected Transform player;
-    protected float myMoveSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform>();
 
-        myMoveSpeed = 6;
-    }
+
+    // Start is called before the first frame update
 
     // Update is called once per frame
     void Update()
     {
-            moveTowardsPlayer();
+        moveTowardsPlayer();
     }
 
-    private void moveTowardsPlayer()
+
+     private void moveTowardsPlayer()
     {
         if (player == null)
         {
@@ -31,19 +26,11 @@ public class EnemyAI : MonoBehaviour
         {
             Vector3 direction = (player.transform.position - transform.position);
             direction = direction.normalized;
-            transform.position += direction * myMoveSpeed * Time.deltaTime;
+            transform.position += direction * 12 * Time.deltaTime;
             if (direction.magnitude >= 1.5f)
             {
                 Debug.LogWarning("Something went wrong with enemy AI movement!");
             }
         }
-    }
-
-
-    public 
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log(" " + other.gameObject.name + " was hit by " + transform.name);
-        Destroy(gameObject);
     }
 }
