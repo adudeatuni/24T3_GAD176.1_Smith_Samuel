039d428a92348de32f6473650f56067a1182372f
adudeatuni <159965809+adudeatuni@users.noreply.github.com>
2024-10-20 12:27:25 +1100

    Weapon stats, and changing them + reloading implimented. Also included is template for creating new sets of gun stats.

--- Assets/Scripts/Gun/GunBase.cs
+++ Assets/Scripts/Gun/GunBase.cs
@@ -1,42 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using GunLogic;
 
+namespace GunLogic
+{
 public class GunBase : MonoBehaviour
 {
-    GunStats stats;
+    private GunStats stats;
     public AudioSource source;
     public AudioClip clip; 
     public Camera playerCam;
-    public bool gunIsAuto = true;
-    bool isFiringGun = false;
-    bool hasAmmoLeft = false;
-   public float gunDamage;
-   public float gunRange;
+    private bool gunIsAuto = true;
+    private bool isFiringGun = false;
+   private float gunDamage;
+   private float gunRange = 0;
+   private float gunFireRate;
 
-    float gunAmmoCapacity;
+    private float gunAmmoCapacity;
 
-    float remainingAmmo;
+   [SerializeField] private float remainingAmmo;
 
-    public string testing = "Revolver";
+    public string currentGun = "None";
 
 void Start ()
 {
     stats = gameObject.GetComponent<GunStats>();
-    stats.Revolver();
-    getGunStats();
+
 }
     // Update is called once per frame
     void Update()
-    {       // Fire1 is a default button set by unity, and if not manually changed, it's set to Mouse 1
+    {   // Fire1 is a default button set by unity, and if not manually changed, it's set to Mouse 1/left Control
      if (Input.GetButtonDown("Fire1"))
      {
+        if (gunRange == 0)
+        {Debug.LogWarning("Player Has no weapon 'equipped'");}
+        else{
         if (gunIsAuto == true)
         {
             StartCoroutine(fullAutoFire());
             isFiringGun = true;
-
+        }
+        else if (gunIsAuto == false)
+        {
+            Shoot();
+        }
         }
 
      } 
@@ -44,12 +51,17 @@ void Start ()
      {
         isFiringGun = false;
      }
+
+     if (Input.GetKeyUp(KeyCode.R))
+     {
+        reload();
+     }
     }
 
-    IEnumerator fullAutoFire()
+    private IEnumerator fullAutoFire()
     {
         Shoot();
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(gunFireRate);
         if (isFiringGun == true)
         {
             StartCoroutine(fullAutoFire());
@@ -60,30 +72,34 @@ void Start ()
 
 
 
-    void Shoot()
+    private void Shoot()
     {
-        source.PlayOneShot(clip);
+ 
         //  When the shoot function is called, shoots out a raycast from the player's camera, with the distance of the "range" value
         //  (this can be changed by changing the playerCam and assigning it to another object, such as a gun, if you want the "bullet" to come from an actual gun)
         //  Stores the info from the object in the private "hitInfo" variable, which can then be used to do things relating to the object hit, such as debugging the name,
         //  or doing math to deal damage
         RaycastHit hitInfo;
-        if(Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hitInfo, gunRange)) 
-        {
-            Debug.Log("Gun Fired!");
-            Debug.Log(hitInfo.transform.name);
-            // Stores a reference to the component "EnemyScript" in the variable "enemyscript"
-            EnemyScript enemyScript = hitInfo.transform.GetComponent<EnemyScript>();
-            // Does a check to see if the hit object has a comonent named "EnemyScript" attatched to it by checking if the enemyScript variable is equal to null or not
-            if (enemyScript != null)
+        if(remainingAmmo > 0)
+        { 
+            source.PlayOneShot(clip);
+            remainingAmmo--;
+            if(Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hitInfo, gunRange)) 
             {
-                enemyScript.enemyHealth = enemyScript.enemyHealth - gunDamage;
-                Debug.Log(" " + hitInfo.transform.name + "was hit and dealt " + gunDamage + "damage. Their remaining health is now" + enemyScript.enemyHealth);
+                Debug.Log("Gun Fired!");
+                Debug.Log(hitInfo.transform.name);
+                // Stores a reference to the component "EnemyScript" in the variable "enemyscript"
+                EnemyScript enemyScript = hitInfo.transform.GetComponent<EnemyScript>();
+                // Does a check to see if the hit object has a comonent named "EnemyScript" attatched to it by checking if the enemyScript variable is equal to null or not
+                if (enemyScript != null)
+                {
+                    enemyScript.enemyHealth = enemyScript.enemyHealth - gunDamage;
+                    Debug.Log(" " + hitInfo.transform.name + "was hit and dealt " + gunDamage + "damage. Their remaining health is now" + enemyScript.enemyHealth);
+             }
             }
-
-            
-
         }
+        else
+        {Debug.Log("No ammo left, reload required!");}
 
     }
 
@@ -92,9 +108,25 @@ void Start ()
 
     public void getGunStats()
     {
+        GunStats transferStats;
+        transferStats = gameObject.GetComponent<GunStats>();
+        gunDamage = transferStats.damage;
+        gunRange = transferStats.range;
+        gunIsAuto = transferStats.isGunFullAuto;
+        gunAmmoCapacity = transferStats.ammoCapacity;
+        remainingAmmo = gunAmmoCapacity;
+        currentGun = transferStats.gunName;
+        gunFireRate = transferStats.fireRate;
         
     }
 
+    private void reload()
+    {
+        remainingAmmo = gunAmmoCapacity;
+        Debug.Log("Gun Reloaded");
+    }
+
+}
 }
 
 
---
+++ Assets/Scripts/Gun/GunStats.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GunLogic;
+
+
+// IMPORTANT NOTE!! 
+// This script must be attatched to the same object as the "GunBase" script!
+public class GunStats : MonoBehaviour
+{
+
+
+    public float damage;
+    public float ammoCapacity;
+    public float range;
+
+    public float fireRate;
+    public bool isGunFullAuto;
+        public string gunName;
+    GunBase gunBase;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gunBase = gameObject.GetComponent<GunBase>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.L))
+        {
+            Revolver();
+        }
+        if(Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            AssaultRifle();
+        }
+        
+    }
+
+
+    public void Revolver()
+    {
+        gunName = "Revolver";
+        damage = 25;
+        ammoCapacity = 6;
+        range = 100;
+        isGunFullAuto = false;
+        Debug.Log("Weapon Changed to Revolver!");
+        gunBase.getGunStats();
+
+        
+
+        
+    }
+
+    public void AssaultRifle()
+    {
+        gunName = "Assault Rifle";
+        damage = 20;
+        ammoCapacity = 31;
+        range = 100;
+        isGunFullAuto = true;
+        fireRate = 0.1f;
+        Debug.Log("Weapon Changed to Assault Rifle!");
+        gunBase.getGunStats();
+    }
+
+
+    // Below is the template for making your own weapon and stats. 
+    // All you need to do is to do is copy/paste the function template,
+    // change the values to your desired values, and call the function
+    // to change the stats of the gun effectivley "swapping" the gun 
+    // the player is using.
+    
+    // NOTE 1: do NOT change "gunBase.getGunStats();" 
+    // that is the function that actually applies the stats to the gun
+
+    // NOTE 2: fireRate is only required if isGunFUllAuto is true, 
+    // Fire rate is in seconds so 0.1f is equal to once every 0.1 seconds
+
+
+    // public void WeaponName()
+    // {
+    //     damage = 20;
+    //     ammoCapacity = 31;
+    //     range = 100;
+    //     isGunFullAuto = true;  
+    //     fireRate = 0.1f;      
+    //     Debug.Log("Message for Console");
+    //     gunBase.getGunStats();
+    // }
+
+
+}
